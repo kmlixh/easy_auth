3de569c401221fc035554082aca76cc9f5f49289
魔人 <kmlixh@foxmail.com>
2025-10-14 20:16:03 +0800

    Auto-commit: 2025-10-14 20:16:03

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import easy_auth
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  EasyAuthPlugin.register(with: registry.registrar(forPlugin: "EasyAuthPlugin"))
+}
